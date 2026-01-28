060519c5941eccdfa8c1fafb061640ce8ebe02fa
luckuslin <lukasl18@nycstudents.net>
2026-01-28 09:24:05 -0500

    stuff idk

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+def squa(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(100)
+squa(200)
+square(400)
+squa(300)
+squa(200)
+square(100)
+turtle.done()
