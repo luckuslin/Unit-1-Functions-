0bfaf1e7e31475fa6c92d51e40ee810057133331
luckuslin <lukasl18@nycstudents.net>
2026-02-05 09:07:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,45 +4,20 @@ t = Turtle()
 
 t.shape('turtle')
 
-def square():
+def square(irange):
  for i in range(4):
     t.forward(100)
     t.left(90) 
 
-""" def triangle():
+def triangle():
    for i in range(3):
      t.forward(100)
      t.left(120)
-   """
-def littlesomething (irange):
-  t.speed(100)
-  length=50
+  
+def littlesomething(irange):
+  length=5
   for i in range(60):
-    t.forward(100)
-    t.left(90)
-    t.left(5)
+    square(length * 5)
+    length += 5
 
-""" def squares(irange):
-  length=50
-  for i in range(irange):
-    t.forward(100)
-    t.left(90)
-    t.left(5)
-    t.forward(5)
-    length += 25 """
-def addSquares(iRange):
-    length = 25
-    for i in range(iRange):
-        square(length, 90)
-        length += 25
-addSquares(5)
-
-
-    
-
-
-
-
-
-
-turtle.done()
+littlesomething(5)
